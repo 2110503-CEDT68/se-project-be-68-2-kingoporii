269208b15d80d134cf97c3829ce92e2c60f819e1
Hirun1414 <ggezhirun@gmail.com>
2026-04-28 14:06:41 +0700

    bug

--- controllers/auth.js
+++ controllers/auth.js
@@ -107,6 +107,7 @@ const sendTokenResponse = (user, statusCode, res)=>{
 
     res.status(statusCode).cookie('token',token,options).json({
         success: true,
+        token,
         _id: user._id,
         name: user.name,
         email: user.email,
